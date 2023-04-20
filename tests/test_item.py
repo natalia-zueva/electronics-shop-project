6c9210c5d5f94fea57f062d0675ba0ba343410c3
Natalia Zueva <zueva_natalia@mail.ru>
2023-04-20 19:52:21 +0500

    correct tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -10,17 +10,12 @@ def test_calculate_total_price():
     assert Item.calculate_total_price(item2) == 100000
 
 
-# def test___init__():
-#     assert Item.all.append() == [<src.item.Item object at 0x000001AC2DBC4310>, <src.item.Item object at 0x000001AC2D958510>]
-
-
 def test_apply_discount():
     item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
-    Item.apply_discount(item1)
+    Item.pay_rate = 0.8
+    item1.apply_discount()
     assert item1.price == 8000.0
-    # assert Item.apply_discount(item1) == 8000.0
-    assert Item.apply_discount(item2) == 10000
-
+    item2 = Item("Ноутбук", 20000, 5)
+    assert item2.price == 20000
 
 
